fb06e1c909983e0ab3116574a063aaa56620ee8f
janandith jayawardena <janandith@192-168-1-4.tpgi.com.au>
2019-04-15 20:20:13 +1000

    Spring Cloud Configuration Server

---
+++ src/main/java/com/gmail/janandith/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.gmail.janandith.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+}
